07f74d68cb04896f6de26012e9b5d0281f7e9894
JoaoPaulo1986 <joao-paulo.ferreira@hotmail.com>
2024-05-04 15:37:37 -0300

    aula Ruby

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -3,7 +3,8 @@ class DebtsController < ApplicationController
 
   # GET /debts or /debts.json
   def index
-    @debts = Debt.all
+    @debts = Debt.paginate(page: params[:page], per_page: 25).includes(:person)
+
   end
 
   # GET /debts/new
